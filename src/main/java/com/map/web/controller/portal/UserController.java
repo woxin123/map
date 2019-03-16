abccd7ae30eb5efd179059fdddfc11aeba1779a1
woxin <1293141942@qq.com>
2019-03-16 15:58:25 +0800

    用户模块重构

--- src/main/java/com/map/web/controller/portal/UserController.java
+++ src/main/java/com/map/web/controller/portal/UserController.java
@@ -37,55 +37,5 @@ public class UserController {
         return userService.getUserMessageById(userId);
 
     }
-//
-//    @GetMapping("/admin/users")
-//    @JsonView(DetialView.class)
-//    public ResultModel getUsers(int pageNo, int pageSize) {
-//
-//        PageInfo<User> list = userService.getUsers(pageNo, pageSize);
-//        List<User> users = list.getList();
-//        System.out.println(list);
-//        if (!users.isEmpty()) {
-//            return ResultBuilder.getSuccess(users);
-//        } else {
-//            return ResultBuilder.getFailure(1, "该范围内没有用户");
-//        }
-//    }
-//
-//    @RequestMapping(value = "/admin/userlock/{userId:\\d+}")
-//    public ResultModel lockUser(@PathVariable int userId) {
-//        return userService.lockedUser(userId);
-//    }
-//
-//    @RequestMapping(value = "/admin/userunlock/{userId:\\d+}")
-//    public ResultModel unLockUser(@PathVariable int userId) {
-//        return userService.unLockUser(userId);
-//    }
-//
-//    @PostMapping(value = "/user")
-//    @JsonView(SimpleView.class)
-//    public ResultModel getUserByName(String username) {
-//        User user = userService.findUserByUsername(username);
-//        if (user == null) {
-//            ResultBuilder.getFailure(1, "用户不存在");
-//        }
-//        return ResultBuilder.getSuccess(user);
-//    }
-//
-//    protected Integer getUserIdFromToken(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        String token = request.getHeader("token");
-//        Integer userId = null;
-//        if (token != null) {
-//            try {
-//                Map<String, Claim> map = JWTUtils.verifyToken(token);
-//                userId = map.get("id").asInt();
-//            } catch (UnsupportedEncodingException e) {
-//                userId = null;
-//            } catch (Exception e) {
-//                request.getRequestDispatcher("/utils/logonExpires").forward(request, response);
-//                e.printStackTrace();
-//            }
-//        }
-//        return userId;
-//    }
+
 }
